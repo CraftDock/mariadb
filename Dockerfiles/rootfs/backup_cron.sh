b1acbca48fc486cf4c007e3e0643a11505f772dd
Hexosse <hexosse@gmail.com>
2022-08-04 13:59:18 +0200

    Change backup folder permission

--- Dockerfiles/rootfs/backup_cron.sh
+++ Dockerfiles/rootfs/backup_cron.sh
@@ -9,6 +9,9 @@ touch /var/log/mysql/backup.log
 chmod 640 /var/log/mysql/backup.log
 chown mysql:mysql /var/log/mysql/backup.log
 
+# Update backup folder permissions
+chown -R mysql:mysql /backup
+
 # Create cron job
 mkdir -p /var/spool/cron/crontabs
 echo "${BACKUP_CRON_TIME} bash /backup.sh >> /var/log/mysql/backup.log 2>&1" > /var/spool/cron/crontabs/mysql
